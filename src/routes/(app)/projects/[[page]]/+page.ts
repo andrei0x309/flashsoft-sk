5ae8d1704fa7039da8f9cd8337a19e14199d9601
Andrei O <andrei@flashsoft.eu>
2022-12-02 16:31:24 +0200

    wip

--- src/routes/(app)/projects/[[page]]/+page.ts
+++ src/routes/(app)/projects/[[page]]/+page.ts
@@ -1,22 +1,8 @@
 import type { PageLoad } from './$types';
-import { API_BASE } from "@/lib/config"
  
 export const load: PageLoad = async ({ params }) => {
-    let req
-    if(params.page) {
-      req = fetch(API_BASE+'/api/projects/get?page='+params.page)
-    } else {
-      req = fetch(API_BASE+'/api/projects/get')
-    }
-    const rReq = await req
-    let data = { error: ''}
-    if(rReq.ok) {
-      data = await rReq.json()
-    } else {
-      data.error = await rReq.text()
-    }
+    const token = Deno.env.get('TOKEN')
     return {
-      data,
-      page: params.page
+      token
     }
 }
