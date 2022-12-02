bd5769b321e7e34627448b467956c75a84c8b79a
Andrei O <andrei@flashsoft.eu>
2022-12-02 15:31:12 +0200

    wip

---
+++ src/routes/(app)/projects/[[page]]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Header from "../../Header.svelte"
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+    console.log(data)
+
+    </script>
+    
+<Header segment="projects" />
