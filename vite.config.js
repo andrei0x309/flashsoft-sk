7a8521f06b4c80b7e0c55f49001af6fbe6ea1713
Andrei O <andrei@flashsoft.eu>
2023-01-17 01:50:51 +0200

    wip

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,11 @@ const config = {
 		VERSION: JSON.stringify(process.env.npm_package_version),
 		SK_VERSION: JSON.stringify(process.env['npm_package_devDependencies__sveltejs_kit'])
 	},
+	build: {
+		rollupOptions: {
+			external: ['smtp-lib']
+		}
+	}
 };
 
 export default config;
