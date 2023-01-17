92c49ff19ea4a61c4ff3a2b881d656b2bcb0bd8f
Andrei O <andrei@flashsoft.eu>
2023-01-17 18:08:41 +0200

    wip

--- edge-functions/email.ts
+++ edge-functions/email.ts
@@ -1,12 +1,7 @@
-import { Config } from "https://edge.netlify.com"
 import { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
 
 const ret = JSON.stringify(SmtpClient)
 
 export default async () => {
    return Response.json({data: ret})
-}
-
-export const config: Config = {
-  path: "/email"
 }
