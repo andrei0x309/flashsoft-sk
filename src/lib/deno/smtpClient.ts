904630aab232a59781c89f5fa7c9e9e2fba59334
Andrei O <andrei@flashsoft.eu>
2023-01-17 03:13:51 +0200

    wip

--- src/lib/deno/smtpClient.ts
+++ src/lib/deno/smtpClient.ts
@@ -1,3 +1,3 @@
-import { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
+const SmtpClient = (await import('https://deno.land/x/smtp@v0.7.0/mod.ts')).SmtpClient;
 
 export const client = new SmtpClient();
