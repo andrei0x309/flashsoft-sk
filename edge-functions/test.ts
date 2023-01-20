6eb5ce52d1e92545e4064de9db70d6e872d80905
Andrei O <andrei@flashsoft.eu>
2023-01-20 18:01:46 +0200

    wip

--- edge-functions/test.ts
+++ edge-functions/test.ts
@@ -3,5 +3,5 @@ export default async (request: Request) => {
     setTimeout(() => { 
         console.log('Hello world')
         }, 1000);
-    new Response.json('Hello world', {status: 200})
+    return Response.json('Hello world', {status: 200})
 }
