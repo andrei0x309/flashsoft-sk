edbbb3e3d0fc40f0e03769e756ec1c8115f55f8a
Andrei O <andrei@flashsoft.eu>
2025-03-12 01:31:26 +0200

    chore: update appcerts fix other index issues

--- src/routes/(app)/certs/[...rest]/+page.server.ts
+++ src/routes/(app)/certs/[...rest]/+page.server.ts
@@ -11,7 +11,7 @@ type PostgesQueryBuilderSelect = ReturnType<PostgesQueryBuilder['select']>
 const loadFilteredCerts = async (page = 1, tag_ids: number[]) => {
   try {
     const countDb = supabase.from('fsk_cert_tag_type').select('count')
-    .in('id', tag_ids)
+      .in('id', tag_ids)
 
     const resDb = supabase.from('fsk_cert').select(`
     id,
@@ -20,18 +20,18 @@ const loadFilteredCerts = async (page = 1, tag_ids: number[]) => {
         name
     )
     `)
-    .in('fsk_cert_tag_type.id', tag_ids)
-    .order('id', {ascending: false})
-    .range((page-1)*9, (page*9)-1)
+      .in('fsk_cert_tag_type.id', tag_ids)
+      .order('id', { ascending: false })
+      .range((page - 1) * 9, (page * 9) - 1)
     let count
     let res
     [count, res] = await Promise.all([countDb, resDb])
     if (count?.data) {
-    count = (count.data as unknown as [{ count: number}]).reduce((a, b) => a + b.count, 0)
+      count = (count.data as unknown as [{ count: number }]).reduce((a, b) => a + b.count, 0)
     } else {
-    count = 0
+      count = 0
     }
-    
+
 
     const ids = res.data?.map((r: { id: number; }) => r.id)
     if (!ids) {
@@ -44,6 +44,7 @@ const loadFilteredCerts = async (page = 1, tag_ids: number[]) => {
     cert_description,
     cert_file_name,
     cert_feature_image,
+    weight,
     cat:fsk_cert_cat(
         cat_name,
         cat_icon_name
@@ -53,32 +54,34 @@ const loadFilteredCerts = async (page = 1, tag_ids: number[]) => {
         name
     )
     `)
-    .in('id', ids)
-          
-    ;(res as unknown as {page: number}).page = page
-    ;(res as unknown as {totalPages: number}).totalPages = Math.ceil((count ?? 0) / 9)
-    ;(res as unknown as {tag_ids: number[]}).tag_ids = tag_ids
-    
-    if(page > (res as unknown as {totalPages: number}).totalPages) {
+      .in('id', ids)
+      .order('weight', { ascending: false })
+      .order('id', { ascending: false })
+
+      ; (res as unknown as { page: number }).page = page
+      ; (res as unknown as { totalPages: number }).totalPages = Math.ceil((count ?? 0) / 9)
+      ; (res as unknown as { tag_ids: number[] }).tag_ids = tag_ids
+
+    if (page > (res as unknown as { totalPages: number }).totalPages) {
       return -1
     }
-    
+
     return {
-        res
-      }; 
-    } catch (e) {
-     return null
-    }
+      res
+    };
+  } catch (e) {
+    return null
   }
+}
 
 const loadCerts = async (page: number, search = false, searchTerms: string[] = []) => {
   try {
     const countDb = optionalChain(supabase
-    .from('fsk_cert')
-    .select('*', { count: 'exact', head: true }))
-    .if(search, 
-      (chain: PostgesQueryBuilderSelect) => 
-      chain.or(searchTerms.map( t => `cert_name.ilike.%${t}%,cert_description.ilike.%${t}%`).join(','))).end()
+      .from('fsk_cert')
+      .select('*', { count: 'exact', head: true }))
+      .if(search,
+        (chain: PostgesQueryBuilderSelect) =>
+          chain.or(searchTerms.map(t => `cert_name.ilike.%${t}%,cert_description.ilike.%${t}%`).join(','))).end()
 
     const resDb = optionalChain(supabase.from('fsk_cert').select(`
   id,
@@ -86,6 +89,7 @@ const loadCerts = async (page: number, search = false, searchTerms: string[] = [
   cert_file_name,
   cert_description,
   cert_feature_image,
+  weight,
   cat:fsk_cert_cat(
       cat_name,
       cat_icon_name
@@ -94,41 +98,43 @@ const loadCerts = async (page: number, search = false, searchTerms: string[] = [
       id,
       name
   )
-  `)).if(search, 
-  (chain: PostgesQueryBuilderSelect) => 
-  chain.or(searchTerms.map( t => `cert_name.ilike.%${t}%,cert_description.ilike.%${t}%`).join(',')))
-  .end()
-  .order('id', {ascending: false})
-  .range((page-1)*9, (page*9)-1)
-  
-  
+  `)).if(search,
+      (chain: PostgesQueryBuilderSelect) =>
+        chain.or(searchTerms.map(t => `cert_name.ilike.%${t}%,cert_description.ilike.%${t}%`).join(',')))
+      .end()
+      .order('weight', { ascending: false, nullsFirst: false })
+      .order('id', { ascending: false })
+      .range((page - 1) * 9, (page * 9) - 1)
+
+
     const [count, res] = await Promise.all([countDb, resDb])
-    
+
     console.log(res)
 
-    ;(res as unknown as {page: number}).page = page
-    ;(res as unknown as {totalPages: number}).totalPages = Math.ceil((count?.count ?? 0) / 9)
-  
-    if(page > (res as unknown as {totalPages: number}).totalPages) {
+      ; (res as unknown as { page: number }).page = page
+      ; (res as unknown as { totalPages: number }).totalPages = Math.ceil((count?.count ?? 0) / 9)
+
+    if (page > (res as unknown as { totalPages: number }).totalPages) {
       return -1
     }
-    
-    return {
-        res
-      }; 
-    } catch (e) {
-     return null
-    }
-  }
 
-  const loadCert = async (id: number, title: string) => {
-    try {
-      const res = await supabase.from('fsk_cert').select(`
+    return {
+      res
+    };
+  } catch (e) {
+    return null
+  }
+}
+
+const loadCert = async (id: number, title: string) => {
+  try {
+    const res = await supabase.from('fsk_cert').select(`
       id,
       cert_name,
       cert_file_name,
       cert_description,
       cert_feature_image,
+      weight,
       cat:fsk_cert_cat(
           cat_name,
           cat_icon_name
@@ -138,76 +144,76 @@ const loadCerts = async (page: number, search = false, searchTerms: string[] = [
           name
       )
       `).eq('id', id)
-      const { data, error } = res
-      if (error) {
-        return null
-      }
-      if (data?.length === 0) {
-        return -1
-      }
-      const cert = data[0]
-      if(makeTitle(cert?.cert_name) !== title) {
-        return -1
-      }
-      return {
-        res: cert
-      }
-    } catch (e) {
+    const { data, error } = res
+    if (error) {
       return null
     }
+    if (data?.length === 0) {
+      return -1
+    }
+    const cert = data[0]
+    if (makeTitle(cert?.cert_name) !== title) {
+      return -1
+    }
+    return {
+      res: cert
+    }
+  } catch (e) {
+    return null
   }
+}
 
 
 export const load: PageServerLoad = async (rest) => {
   const restPath = '/certs/' + (rest.params.rest ?? '/')
-  if(!rest.params.rest) {
+  if (!rest.params.rest) {
     const data = await loadCerts(1)
-    return appendToData(checkData(data), {rest: restPath, pageTitle: 'Courses and Certs | flashsoft.eu', pageDescription: 'Courses and Certs from Linkedin, PluralSight, Udemy, Udacity, Coursera, Edx, etc.'})
+    return appendToData(checkData(data), { rest: restPath, pageTitle: 'Courses and Certs | flashsoft.eu', pageDescription: 'Courses and Certs from Linkedin, PluralSight, Udemy, Udacity, Coursera, Edx, etc.' })
   } else {
     const params = rest.params.rest.split('/')
     console.log(params)
-    switch(params[0]){
+    switch (params[0]) {
       case 'page': {
-      const page = extractPage(rest.params.rest)
+        const page = extractPage(rest.params.rest)
 
-      const data = await loadCerts(page)
-      return appendToData(checkData(data), {rest: restPath, pageTitle: `Courses and Certs | flashsoft.eu | Page ${page}`, pageDescription: 'Courses and Certs from Linkedin, PluralSight, Udemy, Udacity, Coursera, Edx, etc.'})
-    }
-    case 'filter': {
-      const page = extractPage(rest.params.rest)
+        const data = await loadCerts(page)
+        return appendToData(checkData(data), { rest: restPath, pageTitle: `Courses and Certs | flashsoft.eu | Page ${page}`, pageDescription: 'Courses and Certs from Linkedin, PluralSight, Udemy, Udacity, Coursera, Edx, etc.' })
+      }
+      case 'filter': {
+        const page = extractPage(rest.params.rest)
 
-      const tag_ids = rest.params.rest.split('/tags/')[1].split('/').map((t) => Number(t)).filter((t) => !isNaN(t))
-      const data =  await loadFilteredCerts(page, tag_ids)
-      return appendToData(checkData(data), {rest: restPath, pageTitle: `Courses and Certs | flashsoft.eu | Filter tag: ${[tag_ids[0], tag_ids[1], tag_ids[2]].filter(e => !!e).join(' ') }`, pageDescription: 'Filter for courses and Certs from Linkedin, PluralSight, Udemy, Udacity, Coursera, Edx, etc.'})
-    }
-    case 'view': {
-      console.log(params[1], params[2])
-      const id = Number(params[1])
-      if(isNaN(id)) {
-        throw error(404, 'Not found')
+        const tag_ids = rest.params.rest.split('/tags/')[1].split('/').map((t) => Number(t)).filter((t) => !isNaN(t))
+        const data = await loadFilteredCerts(page, tag_ids)
+        return appendToData(checkData(data), { rest: restPath, pageTitle: `Courses and Certs | flashsoft.eu | Filter tag: ${[tag_ids[0], tag_ids[1], tag_ids[2]].filter(e => !!e).join(' ')}`, pageDescription: 'Filter for courses and Certs from Linkedin, PluralSight, Udemy, Udacity, Coursera, Edx, etc.' })
       }
-      const title = params[2]
-      if(!title) {
-        throw error(404, 'Not found')
-      }
-      
-      const data = await loadCert(id, title) as {res: Record<string, unknown>}
-      return appendToData(checkData(data), {rest: restPath, pageTitle: data?.res?.cert_name ?? 'Courses and Certs | flashsoft.eu', pageDescription: data?.res?.cert_description || `Course details ${data?.res?.cert_name ? 'for ' + data?.res?.cert_name : ''}`})
+      case 'view': {
+        console.log(params[1], params[2])
+        const id = Number(params[1])
+        if (isNaN(id)) {
+          throw error(404, 'Not found')
+        }
+        const title = params[2]
+        if (!title) {
+          throw error(404, 'Not found')
+        }
+
+        const data = await loadCert(id, title) as { res: Record<string, unknown> }
+        return appendToData(checkData(data), { rest: restPath, pageTitle: data?.res?.cert_name ?? 'Courses and Certs | flashsoft.eu', pageDescription: data?.res?.cert_description || `Course details ${data?.res?.cert_name ? 'for ' + data?.res?.cert_name : ''}` })
       }
       case 'search': {
-      const page = extractPage(rest.params.rest)
-      const searchTerms =  rest.url.searchParams.getAll('q')
-      console.log(searchTerms)
-      return appendToData(checkData(await loadCerts(page, true, searchTerms)), {
-        rest: restPath,
-        searchInput: searchTerms.join(' '),
-        pageTitle: 'Searching cert for ' + searchTerms.join(' ').substring(0, 20) + '...',
-        pageDescription: 'Searching cert for ' + searchTerms.join(' ').substring(0, 170) + '...'
-      })
-    }
-    default: {
-      throw error(404, 'Not found')
+        const page = extractPage(rest.params.rest)
+        const searchTerms = rest.url.searchParams.getAll('q')
+        console.log(searchTerms)
+        return appendToData(checkData(await loadCerts(page, true, searchTerms)), {
+          rest: restPath,
+          searchInput: searchTerms.join(' '),
+          pageTitle: 'Searching cert for ' + searchTerms.join(' ').substring(0, 20) + '...',
+          pageDescription: 'Searching cert for ' + searchTerms.join(' ').substring(0, 170) + '...'
+        })
+      }
+      default: {
+        throw error(404, 'Not found')
+      }
     }
   }
- }
 }
